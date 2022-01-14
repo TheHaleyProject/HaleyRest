9dd7bc18ebdfd45d0026df4bf763bc9d3a2e127f
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2022-01-14 23:28:01 +0400

    Basic tests

--- RestExamples/MainWindow.xaml.cs
+++ RestExamples/MainWindow.xaml.cs
@@ -12,13 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Haley.WPF.Controls;
 
 namespace RestExamples
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : PlainWindow
     {
         public MainWindow()
         {
