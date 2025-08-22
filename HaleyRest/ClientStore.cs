29b93c8f17e09f0334ae9cbd4656a9a64222d9af
LingamHPZ4 <rmsmech@gmail.com>
2025-08-22 13:38:15 +0400

    ssl ignore added

--- HaleyRest/ClientStore.cs
+++ HaleyRest/ClientStore.cs
@@ -1,9 +1,11 @@
 ﻿using Haley.Abstractions;
 using Haley.Models;
 using Haley.Utils;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -22,6 +24,38 @@ namespace Haley.Rest {
         }
         public static IClient GetClient(Enum @enum) {
             return GetClient(@enum.GetKey());
+        }
+
+        public static IClient GenerateClient(Dictionary<string, object> dic, ILogger logger) {
+            HttpMessageHandler handler = null;
+            if (dic.ContainsKey("ssl-ignore")) handler = new HttpClientHandler { ServerCertificateCustomValidationCallback = (m, c, ch, e) => true };
+            var result = new FluentClient(dic.GenerateBaseURLAddress(), logger, handler) { };
+            result.BaseClient.Timeout = TimeSpan.FromSeconds(200);
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="cfgInfo">Holding data like : "base=http://{{app-ip-or-url}}:{{port}}/;route={{/SOME-SUFFIX}};suffix={{SOME-ADDITIONAL-SUFFIX}};ssl-ignore;"</param>
+        /// <param name="logger"></param>
+        /// <returns></returns>
+        public static IClient AddClient(Enum key, string cfgInfo, ILogger logger = null) {
+            return AddClient(key.GetKey(), cfgInfo, logger);
+        }
+       
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="cfgInfo">Holding data like : "base=http://{{app-ip-or-url}}:{{port}}/;route={{/SOME-SUFFIX}};suffix={{SOME-ADDITIONAL-SUFFIX}};ssl-ignore;"</param>
+        /// <param name="logger"></param>
+        /// <returns></returns>
+        public static IClient AddClient(string key, string cfgInfo, ILogger logger = null) {
+            if (string.IsNullOrWhiteSpace(cfgInfo)) return null;
+            var dic = cfgInfo.ToDictionarySplit();
+            return AddClient(key, GenerateClient(dic, logger));
         }
 
         public static IClient AddClient(Enum @enum, IClient client) { return AddClient(@enum.GetKey(), client); }
